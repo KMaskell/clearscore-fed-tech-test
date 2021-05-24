da5c5bf84f2be6695f04d09d00ecd1055694be7e
Kat Maskell <kat.maskell@clearscore.com>
2021-05-24 17:21:30 +0100

    feat(insights) render on/off track pill for electoral role

--- src/Insights.js
+++ src/Insights.js
@@ -8,9 +8,12 @@ import language from './language';
 import data from './mock-report.json';
 
 const Insights = () => {
-    // console.log('JSON data', data);
     const accounts = Object.values(data.accounts).map((accountCategoryKey) => {
         return accountCategoryKey;
+    });
+
+    const insightSegment = Object.keys(language).map((insightKey) => {
+        return language[insightKey];
     });
 
     const creditUtilisationPill = Object.values(accounts).map((account) => {
@@ -25,35 +28,47 @@ const Insights = () => {
         }
     });
 
-    const insights = Object.keys(language).map((insightKey) => {
-        const insightSegment = language[insightKey];
-        return (
-            <ol key={insightKey} className="insight-card">
-                <li key={insightKey}>
-                    {insightSegment.Header === 'Public information' ? (
-                        data.personal.publicInfo.courtAndInsolvencies ? (
-                            <OffTrackToken />
-                        ) : (
-                            <OnTrackToken />
-                        )
-                    ) : null}
-                    {insightSegment.Header === 'Credit utilisation' &&
-                        creditUtilisationPill}
-                    <h6>{insightSegment.Header}</h6>
-                    <p>{insightSegment.Body}</p>
-                    {insightSegment.Impact === 'High Impact' ? (
-                        <HighImpactPill />
-                    ) : (
-                        <MedImpactPill />
-                    )}
-                </li>
-            </ol>
-        );
-    });
+    const electoralRollPill = Object.values(data.personal.electoralRoll).map(
+        (personalValue) => {
+            if (personalValue.current === true) {
+                return <OnTrackToken />;
+            } else {
+                return <OffTrackToken />;
+            }
+        }
+    );
     return (
         <div className="wrapper">
             <h1 className="heading">Insights</h1>
-            <div className="cardContainer">{insights}</div>
+            {insightSegment.map((insight) => {
+                return (
+                    <ol key={insight.Header} className="insight-card">
+                        <li key={insight.Header}>
+                            {insight.Header === 'Public information' ? (
+                                data.personal.publicInfo
+                                    .courtAndInsolvencies ? (
+                                    <OffTrackToken />
+                                ) : (
+                                    <OnTrackToken />
+                                )
+                            ) : null}
+                            {insight.Header === 'Credit utilisation' &&
+                                creditUtilisationPill}
+                            {insight.Header === 'Electoral roll' &&
+                                electoralRollPill}
+                            <h4 className="insight-heading">
+                                {insight.Header}
+                            </h4>
+                            <p>{insight.Body}</p>
+                            {insight.Impact === 'High Impact' ? (
+                                <HighImpactPill />
+                            ) : (
+                                <MedImpactPill />
+                            )}
+                        </li>
+                    </ol>
+                );
+            })}
         </div>
     );
 };
