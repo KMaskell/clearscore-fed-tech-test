73b51cbc260450415a0d9ae122c4434e489993ab
Kat Maskell <kat.maskell@clearscore.com>
2021-05-24 14:24:45 +0100

    feat(insights) render on/off track pill for public info insight

--- src/Insights.js
+++ src/Insights.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './styles/insights.css';
-// import OnTrackToken from './components/on-track-token';
-// import OffTrackToken from './components/off-track-token';
+import OnTrackToken from './components/on-track-token';
+import OffTrackToken from './components/off-track-token';
 import MedImpactPill from './components/med-impact-pill';
 import HighImpactPill from './components/high-impact-pill';
 import language from './language';
@@ -14,11 +14,15 @@ const Insights = () => {
     const insights = Object.keys(language).map((insightKey) => {
         const insightSegment = language[insightKey];
         return (
-            <ol key={insightKey}>
-                <li className="insight-card">
-                    {insightSegment.Header === 'Public information'
-                        ? console.log('yes')
-                        : console.log('no')}
+            <ol key={insightKey} className="insight-card">
+                <li>
+                    {insightSegment.Header === 'Public information' ? (
+                        data.personal.publicInfo.courtAndInsolvencies ? (
+                            <OffTrackToken />
+                        ) : (
+                            <OnTrackToken />
+                        )
+                    ) : null}
                     <h6>{insightSegment.Header}</h6>
                     <p>{insightSegment.Body}</p>
                     {insightSegment.Impact === 'High Impact' ? (
